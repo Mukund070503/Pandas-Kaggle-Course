ec0e4ca1ed8918f99efca99bb6e0fc419fc7de16
Mukund070503 <113293574+Mukund070503@users.noreply.github.com>
2026-02-26 18:33:21 +0530

    Update File1.py

--- File1.py
+++ File1.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
-pd.DataFrame({'Name':["Mukund","Madhav"],'Score':[80,75]},index=[1,2])
+#pandas have 2 objects DataFrame and Series
+#DataFrame are tables with an array of entities. Each entities have values.
+#Series is a collection of values like a list. It has only one column and it does not have a column name it can only have an overall name
+
+pd.DataFrame({'Name':["Mukund","Madhav"],'Score':[80,75]},index=[1,2])  #index is not compulsory but in case of default it will start from 0, it can also be string
