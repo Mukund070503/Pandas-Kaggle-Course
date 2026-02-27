b3df7ddaaad76b7c4a9381df7543c3981418a59b
Mukund070503 <113293574+Mukund070503@users.noreply.github.com>
2026-02-27 11:41:45 +0530

    Update File1.py

--- File1.py
+++ File1.py
@@ -5,3 +5,12 @@ import pandas as pd
 #Series is a collection of values like a list. It has only one column and it does not have a column name it can only have an overall name
 
 pd.DataFrame({'Name':["Mukund","Madhav"],'Score':[80,75]},index=[1,2])  #index is not compulsory but in case of default it will start from 0, it can also be string
+
+pd.Series([10,20,30], index=['value1','value2','value3'],name='Points') #Here index and name is not compulsory in default case index will start with 0
+
+dataSet = pd.read_csv("Path") #This has over 30 parameters that can be specified for Example if data set has in-built index column
+#we need to specify that otherwise a new col will be created like this dataSet = pd.read_csv("Path",index_col=0)
+
+dataSet.shape
+dataSet.head()
+dataSet.to_csv('name') # to create a csv file and save it 
